8776a746af30f2442b6410683a8fb59303b423b6
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2025-06-22 18:19:20 -0400

    got stuck because property editor doesn't seem to be in the public api

---
+++ src/main.ts
@@ -0,0 +1,92 @@
+import {
+	App,
+	Editor,
+	MarkdownView,
+	Modal,
+	Notice,
+	Plugin,
+	PluginSettingTab,
+	Setting,
+} from "obsidian";
+
+// Remember to rename these classes and interfaces!
+
+interface ColourPropertyPluginSettings {
+	mySetting: string;
+}
+
+const DEFAULT_SETTINGS: ColourPropertyPluginSettings = {
+	mySetting: "default",
+};
+
+export default class ColourPropertyPlugin extends Plugin {
+	settings: ColourPropertyPluginSettings;
+
+	async onload() {
+		await this.loadSettings();
+
+		this.registerEvent(
+			this.app.workspace.on("editor-menu", (menu, file) => {
+				console.log(menu);
+			})
+		);
+	}
+
+	onunload() {}
+
+	async loadSettings() {
+		this.settings = Object.assign(
+			{},
+			DEFAULT_SETTINGS,
+			await this.loadData()
+		);
+	}
+
+	async saveSettings() {
+		await this.saveData(this.settings);
+	}
+}
+
+class SampleModal extends Modal {
+	constructor(app: App) {
+		super(app);
+	}
+
+	onOpen() {
+		const { contentEl } = this;
+		contentEl.setText("Woah!");
+	}
+
+	onClose() {
+		const { contentEl } = this;
+		contentEl.empty();
+	}
+}
+
+class ColourPropertyPluginSettingTab extends PluginSettingTab {
+	plugin: ColourPropertyPlugin;
+
+	constructor(app: App, plugin: ColourPropertyPlugin) {
+		super(app, plugin);
+		this.plugin = plugin;
+	}
+
+	display(): void {
+		const { containerEl } = this;
+
+		containerEl.empty();
+
+		new Setting(containerEl)
+			.setName("Setting #1")
+			.setDesc("It's a secret")
+			.addText((text) =>
+				text
+					.setPlaceholder("Enter your secret")
+					.setValue(this.plugin.settings.mySetting)
+					.onChange(async (value) => {
+						this.plugin.settings.mySetting = value;
+						await this.plugin.saveSettings();
+					})
+			);
+	}
+}
